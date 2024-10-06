62d9b4200c54bb0f7b7ed58904720f3f035b80e8
Paul Wicking <paul.wicking@qt.io>
2024-10-06 19:33:11 +0200

    Update Clang libraries for QDoc to 19.1.7
    * Update the version variable used to identify the correct package for
      download and cache sites.
    * Update the file names for all platforms, as these libraries are built
      directly from upstream without other patches applied on top.
    * Add provisioning package locations for Windows VS2022 x64.
    * Set 'vs2022' as default toolchain on Windows.
    * Update call sites for `libclang.ps1` to pass `vs2022` as argument to
      the script.
    * Drop Windows configurations with VS2019.
    * Update cached and official URLs with the major version for Windows
      platforms.
    * Update SHA1 for provisioning packages on all platforms.
    * Move from RHEL 8.8 to 8.10.
    * Move from RHEL 9.2 to 9.4.
    
    Fixes: QTBUG-129506
    Fixes: QTQAINFRA-6605
    Task-number: QTBUG-129373
    Pick-to: 6.8
    Change-Id: I5146e6551a1b3c83133803f61ca6f8428cb6a14f
    Reviewed-by: Topi Reiniö <topi.reinio@qt.io>
    (cherry picked from commit 9ff9d34626bd65c6dd448ed0e232df58a72a0052)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- coin/provisioning/common/windows/libclang.ps1
+++ coin/provisioning/common/windows/libclang.ps1
@@ -1,11 +1,11 @@
 param(
     [string]$archVer="32",
-    [string]$toolchain="vs2019",
+    [string]$toolchain="vs2022",
     [bool]$setDefault=$true
 )
 . "$PSScriptRoot\helpers.ps1"
 
-$libclang_version="18.1.7"
+$libclang_version="19.1.7"
 Write-Output "libClang = $libclang_version" >> ~/versions.txt
 
 # PySide versions following 5.6 use a C++ parser based on Clang (http://clang.org/).
@@ -24,8 +24,8 @@ function install() {
 
     $zip = "c:\users\qt\downloads\libclang.7z"
 
-    $script:OfficialUrl = "https://download.qt.io/development_releases/prebuilt/libclang/qt/libclang-release_$libclang_version-based-windows-$toolchain`_$archVer.7z"
-    $script:CachedUrl = "http://ci-files01-hki.ci.qt.io/input/libclang/qt/libclang-release_$libclang_version-based-windows-$toolchain`_$archVer.7z"
+    $script:OfficialUrl = "https://download.qt.io/development_releases/prebuilt/libclang/qt/libclang-llvmorg-$libclang_version-windows-$toolchain`_$archVer.7z"
+    $script:CachedUrl = "http://ci-files01-hki.ci.qt.io/input/libclang/qt/libclang-llvmorg-$libclang_version-windows-$toolchain`_$archVer.7z"
 
     Download $OfficialUrl $CachedUrl $zip
     Verify-Checksum $zip $sha1
@@ -38,20 +38,10 @@ $toolchainSuffix = ""
 
 if ( $toolchain -eq "vs2022" ) {
     if ( $archVer -eq "64" ) {
-        $sha1 = "7e51f0eabdfe8eea17aaf1dce7b2ffe1ea064f66"
+        $sha1 = "f56057b8679e21a44b341bb1041cb03fbe6f5c0d"
     }
     elseif ( $archVer -eq "arm64" ) {
-        $sha1 = "986d4d0f253de505ef499345238c101dac1ca3a6"
-    }
-    else {
-        $sha1 = ""
-    }
-    $toolchainSuffix = "msvc"
-}
-
-if ( $toolchain -eq "vs2019" ) {
-    if ( $archVer -eq "64" ) {
-        $sha1 = "8e0862386caef7e4537599ef980eeb6ebee8767f"
+        $sha1 = "89fddd8c4bde3e8b70382e21059743637c27d38d"
     }
     else {
         $sha1 = ""
@@ -61,7 +51,7 @@ if ( $toolchain -eq "vs2019" ) {
 
 if ( $toolchain -eq "mingw" ) {
     if ( $archVer -eq "64" ) {
-        $sha1 = "a23cbb0822cf2eb8d1cecf26e8614ef37a7611e3"
+        $sha1 = "fcc1f06bd395bc133b7828d0be48e8492b9ba807"
     }
     else {
         $sha1 = ""
@@ -72,7 +62,7 @@ if ( $toolchain -eq "mingw" ) {
 
 if ( $toolchain -eq "llvm-mingw" ) {
     if ( $archVer -eq "64" ) {
-        $sha1 = "9c34f99eb575b42c2befe27829c08e6d3f01ae58"
+        $sha1 = "ee01352eb68bee252cefb1b8ff4ad086baa8ab5f"
     }
     else {
         $sha1 = ""
