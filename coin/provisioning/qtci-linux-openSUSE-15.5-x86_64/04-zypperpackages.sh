01f6c96002bf3eecc7500a58a9dec1f286d48cba
Tim Blechmann <tim@klingt.org>
2024-11-08 09:09:05 +0800

    provisioning: install development files for gstreamer-plugins-bad
    Required for gst-play
    
    Pick-to: 6.8
    Change-Id: Id41d4359aa8e573e360c565eea6900b33918743f
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>

--- coin/provisioning/qtci-linux-openSUSE-15.5-x86_64/04-zypperpackages.sh
+++ coin/provisioning/qtci-linux-openSUSE-15.5-x86_64/04-zypperpackages.sh
@@ -49,7 +49,8 @@ sudo zypper -nq install libxml2-devel libxslt-devel
 sudo zypper -nq install yasm
 
 # GStreamer (qtwebkit and qtmultimedia), pulseaudio (qtmultimedia)
-sudo zypper -nq install gstreamer-devel gstreamer-plugins-base-devel libpulse-devel pipewire-devel gstreamer-1.20-plugin-openh264
+sudo zypper -nq install gstreamer-devel gstreamer-plugins-base-devel libpulse-devel pipewire-devel \
+         gstreamer-1.20-plugin-openh264 gstreamer-plugins-bad-devel
 
 # cups
 sudo zypper -nq install cups-devel
